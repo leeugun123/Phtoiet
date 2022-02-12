a78673dee4733556c93cb4759c7b81a26a605503
이유건 <50404123+leeugun123@users.noreply.github.com>
2022-02-12 20:45:18 +0900

    fragment

--- app/src/main/java/org/techtown/phtoiet/Menu_Activity.kt
+++ app/src/main/java/org/techtown/phtoiet/Menu_Activity.kt
@@ -5,21 +5,42 @@ import android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.kakao.sdk.user.UserApiClient
+import org.techtown.phtoiet.databinding.ActivityMenuBinding
 
 class Menu_Activity : AppCompatActivity() {
+
+    lateinit var binding:ActivityMenuBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        //setContentView(R.layout.activity_menu)
+
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.button1.setOnClickListener{
+            setFragment1()
+        }
 
         val nickname = findViewById<TextView>(R.id.nickname)//닉네임
 
         UserApiClient.instance.me { user, error ->
-            "닉네임: ${user?.kakaoAccount?.profile?.nickname}".also { nickname.text = it }
+            "${user?.kakaoAccount?.profile?.nickname}".also { nickname.text = it }
         }//카카오에서 닉네임 불러오기
 
+
+        val profile = findViewById<ImageView>(R.id.profile)
+
+        UserApiClient.instance.me { user, error ->
+            Glide.with(profile).load(user?.kakaoAccount?.profile?.profileImageUrl).circleCrop().into(profile)
+        }//카카오톡 프로필 이미지 가져오기
 
         /*
         val kakao_logout_button = findViewById<Button>(R.id.logout_button) // 로그인 버튼
@@ -52,6 +73,15 @@ class Menu_Activity : AppCompatActivity() {
             }
         }
         */
+
+    }
+
+    fun setFragment1(){
+
+        val transaction = supportFragmentManager.beginTransaction()
+            .add(R.id.frame,Today_fragment())
+
+        transaction.commit()
 
     }
 }
