42e0f1e074e39c88e8d54fdb0b27cb8b1b54f712
이유건 <50404123+leeugun123@users.noreply.github.com>
2022-03-01 00:24:07 +0900

    firebase implementation(2)

--- app/src/main/java/org/techtown/phtoiet/Plus_Fragment.kt
+++ app/src/main/java/org/techtown/phtoiet/Plus_Fragment.kt
@@ -11,70 +11,71 @@ import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_plus_.*
 import org.techtown.phtoiet.databinding.ActivityMainBinding
 
 class Plus_Fragment : Fragment() {
 
+
+    private lateinit var binding: ActivityMainBinding
     lateinit var recyclerView: RecyclerView
     lateinit var mainActivity: MainActivity
     //private lateinit var binding : ActivityMainBinding
 
-    val db = FirebaseFirestore.getInstance()
+    val db = Firebase.firestore
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
         if(context is MainActivity) mainActivity = context
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
 
         val rootView = inflater.inflate(R.layout.fragment_plus_, container, false)
-
-
-        var foodList = ArrayList<Profiles>()//food의 ArrayList
-
-
-        /*
-        foodList.add(Profiles(R.drawable.noddle,"짜장면","803.4","12시 30분"))
-        foodList.add(Profiles(R.drawable.cutlet,"돈가스","800.3","13시 20분"))
-        */
-
-        recyclerView = rootView.findViewById(R.id.list)as RecyclerView
-        recyclerView.layoutManager =
-            LinearLayoutManager(requireContext())
-
-
-
-            db.collection("Contacts")//작업할 컬렉션
-                .get()  //문서 가져오기
-                .addOnSuccessListener {result ->
-                    //성공할경우
-                    foodList.clear()
-                        for(document in result){ val item = Profiles(document["img"] as String,document["food_name"] as String,document["calories"] as String, document["time"] as String)
-                        foodList.add(item)
-                    }
-                }
-                .addOnFailureListener{
-                    //실패할경우
-                    Toast.makeText(getActivity(),"Toast Message",Toast.LENGTH_SHORT).show();
-
-                 }
-
-
-
-        recyclerView.adapter =
-            ProfileAdapter(requireContext(),foodList)
-
-
         return rootView
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+        var foodList = ArrayList<Profiles>()//food의 ArrayList
+
+        foodList.add(Profiles("eeeee","짜장면","803.4","12시 30분"))
+        foodList.add(Profiles("eeeeee","돈가스","800.3","13시 20분"))
+
+
+        recyclerView = getView()?.findViewById(R.id.list) as RecyclerView
+        recyclerView.layoutManager =
+            LinearLayoutManager(requireContext())
+
+
+
+        db.collection("Contacts")//작업할 컬렉션
+            .get()  //문서 가져오기
+            .addOnSuccessListener {result ->
+                //성공할경우
+
+                foodList.clear()
+                for(document in result){ val item = Profiles(document["calories"] as String,document["food_name"] as String,document["img"] as String, document["time"] as String)
+                    foodList.add(item)
+                    Log.d("TAG","여기까지 올려나???")
+                }
+
+            }
+            .addOnFailureListener{
+                //실패할경우
+
+                Toast.makeText(getActivity(),"Toast Message",Toast.LENGTH_SHORT).show()
+            }
+
+
+
+        recyclerView.adapter =
+            ProfileAdapter(requireContext(),foodList)
 
 
     }
