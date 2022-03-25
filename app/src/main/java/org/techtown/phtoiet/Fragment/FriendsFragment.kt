33ae396d36ffb3d07b5680c70e88ba601a0f408d
이유건 <50404123+leeugun123@users.noreply.github.com>
2022-03-26 01:03:25 +0900

    사진 찍기 기능 구현 사진 -> ImageView 붙이기

--- app/src/main/java/org/techtown/phtoiet/Fragment/FriendsFragment.kt
+++ app/src/main/java/org/techtown/phtoiet/Fragment/FriendsFragment.kt
@@ -1,11 +1,19 @@
 package org.techtown.phtoiet.Fragment
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.os.Environment.DIRECTORY_PICTURES
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.FileProvider
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.DataBindingUtil.setContentView
 import androidx.fragment.app.Fragment
@@ -14,32 +22,37 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.fragment_friends.*
+import kotlinx.android.synthetic.main.recycler_view_test.view.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.techtown.phtoiet.*
 import org.techtown.phtoiet.databinding.ActivityMenuBinding
 import org.techtown.phtoiet.databinding.FragmentFriendsBinding
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 //오늘의 일정 확인 하는 Fragment
 class FriendsFragment : Fragment() , OnItemClick {
 
-        private val model: MealViewModel by viewModels()
-        private lateinit var adapter: MealAdapter
-        private lateinit var binding : FragmentFriendsBinding
+    private val model: MealViewModel by viewModels()
+    private lateinit var adapter: MealAdapter
+    private lateinit var binding : FragmentFriendsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = FragmentFriendsBinding.inflate(layoutInflater)
+        //데이터 바인딩
 
         initRecyclerView()//리사이클러뷰 초기화
-
 
         model.getAll().observe(this, Observer {
             adapter.setList(it)
             adapter.notifyDataSetChanged()
         })//observer를 붙여서 데이터가 변경되면, adapter에게 넘기고 새로고침
-
 
 
         binding.mPlusButton.setOnClickListener{
@@ -59,7 +72,6 @@ class FriendsFragment : Fragment() , OnItemClick {
         binding.mRecyclerView.adapter = adapter
     }
 
-
     override fun onDestroyView() {
         super.onDestroyView()
     }
@@ -73,6 +85,7 @@ class FriendsFragment : Fragment() , OnItemClick {
             model.delete(meal)
         }
     }
+
 
 
 }
