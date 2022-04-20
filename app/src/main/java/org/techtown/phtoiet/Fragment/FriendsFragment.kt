83a0b23fd83182da835dffcdad25bbb584b7b1c4
이유건 <50404123+leeugun123@users.noreply.github.com>
2022-04-20 22:41:26 +0900

    사진 찍기 fragment에서 적용 중..

--- app/src/main/java/org/techtown/phtoiet/Fragment/FriendsFragment.kt
+++ app/src/main/java/org/techtown/phtoiet/Fragment/FriendsFragment.kt
@@ -41,6 +41,7 @@ class FriendsFragment : Fragment() , OnItemClick {
     private lateinit var adapter: MealAdapter
     private lateinit var binding : FragmentFriendsBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -49,28 +50,18 @@ class FriendsFragment : Fragment() , OnItemClick {
 
         initRecyclerView()//리사이클러뷰 초기화
 
+
+
         model.getAll().observe(this, Observer {
             adapter.setList(it)
             adapter.notifyDataSetChanged()
         })//observer를 붙여서 데이터가 변경되면, adapter에게 넘기고 새로고침
 
-
         binding.mPlusButton.setOnClickListener{
 
-            lifecycleScope.launch(Dispatchers.IO){
-
-                activity?.let {
-
-                    val intent = Intent(context,Upload_meal::class.java)
-                    startActivity(intent)
+            model.insert(Meal("https://cdn.pixabay.com/photo/2021/08/03/07/03/orange-6518675_960_720.jpg","오렌지","10시 30분","345"))
 
 
-                }//Upload_mill로 화면전환 (설정할 수 있는 화면으로 이동 가능)
-
-
-
-                //model.insert(Meal("https://cdn.pixabay.com/photo/2021/08/03/07/03/orange-6518675_960_720.jpg","오렌지","10시 30분","345"))
-            }//메인 쓰레드에서 실행하면 안되므로 코루틴 사용??
 
         //editext로 내용을 받아 입력 구현하기
 
@@ -79,12 +70,16 @@ class FriendsFragment : Fragment() , OnItemClick {
     }
 
     private fun initRecyclerView(){
+
         adapter = MealAdapter(this)
         binding.mRecyclerView.adapter = adapter
+
     }
 
     override fun onDestroyView() {
+
         super.onDestroyView()
+
     }
 
     override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?) : View?{
